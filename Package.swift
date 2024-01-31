c9f354e64423fa6f39732aed10d03b8a69dd5c2a
Sidharth <sidharth@vizbee.tv>
2024-01-31 11:52:34 +0530

    Reverted the dependencies

--- Package.swift
+++ Package.swift
@@ -1,24 +1,23 @@
-// swift-tools-version: 5.9
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "VizbeeHomeSSOKit",
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "VizbeeHomeSSOKit",
             targets: ["VizbeeHomeSSOKit"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/ClaspTV/vizbee-ios-sdk.git", from: "6.5.1-alpha"),
-        // Add other dependencies as needed
-    ], targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+    targets: [
         .binaryTarget(
             name: "VizbeeHomeSSOKit",
-            path: "VizbeeHomeSSOKit.xcframework")
+            path: "VizbeeHomeSSOKit.xcframework"
+        )
     ]
 )
